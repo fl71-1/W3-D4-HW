a3516baf3db596163944215eaa9791af6c56a7e1
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-12 09:11:36 +0300

    W3-D4-HW

---
+++ script.js
@@ -0,0 +1,109 @@
+document.getElementById('name1').onmouseover = function() {
+    this.textContent = 'FAHAD ALQHTANI'; 
+};
+
+document.getElementById('work').onmouseover = function() {
+    this.textContent = 'Recent graduation'; 
+};
+document.getElementById('profile-text').onmouseover = function() {
+    this.textContent = 'I graduated this year from Majmaah University with a degree in Computer Science and Information. I concluded my studies with university training at King Abdulaziz City for Science and Technology under the supervision of Boeing in the aviation and defense industries, specifically in the Decision Support and Decision-Making department. I trained in building web applications using JavaScript, which directed me towards the vast field of programming, focusing specifically on JavaScript. I am currently a student in the Web Development Bootcamp using JavaScript at Tuwaiq Academy, aspiring to perform well and achieve my goals. I aim to further develop myself, and I feel fortunate to have such an amazing educational staff.'; 
+};
+document.getElementById('work1').onmouseover = function() {
+    this.innerHTML = 'KACST<br><small>King Abdulaziz City for Science and Technology</small>'; 
+};
+document.getElementById('ago1').onmouseover = function() {
+    this.textContent = '2024'; 
+};
+document.getElementById('li1').onmouseover = function() {
+    this.textContent = 'Creating and developing the web using JavaScript'; 
+};
+document.getElementById('li2').onmouseover = function() {
+    this.textContent = 'Software project management'; 
+};
+document.getElementById('li3').onmouseover = function() {
+    this.textContent = 'Create an interactive platform'; 
+};
+document.getElementById('work2').onmouseover = function() {
+    this.innerHTML = 'BOEING<br><small>Cooperative Training Program with Decision Support Center (DSC)</small>'; 
+};
+document.getElementById('ago2').onmouseover = function() {
+    this.textContent = '2024'; 
+};
+document.getElementById('li4').onmouseover = function() {
+    this.textContent = 'Creating and developing the web using JavaScript'; 
+};
+document.getElementById('li5').onmouseover = function() {
+    this.textContent = 'Software project management'; 
+};
+document.getElementById('li6').onmouseover = function() {
+    this.textContent = 'Create an interactive platform'; 
+};
+document.getElementById('work3').onmouseover = function() {
+    this.innerHTML = 'Majmaah University<br><small>Computer Science and Information</small>'; 
+};
+document.getElementById('ago2').onmouseover = function() {
+    this.textContent = '2024 - 2019'; 
+};
+document.getElementById('li7').onmouseover = function() {
+    this.textContent = 'Creating and developing the web using HTML , CSS'; 
+};
+document.getElementById('li8').onmouseover = function() {
+    this.textContent = ' study five years Bachelor in Computer Science'; 
+};
+
+document.getElementById('number').onmouseover = function() {
+    this.textContent = '📞 : +966 553601466'; 
+};
+document.getElementById('email').onmouseover = function() {
+    this.textContent = '✉️ : fahdalghtni246@gmail.com'; 
+};
+document.getElementById('address').onmouseover = function() {
+    this.textContent = '🏠 : Central Region, Riyadh'; 
+};
+document.getElementById('web').onmouseover = function() {
+    this.innerHTML = '🌐 : <a href="https://www.linkedin.com/in/fahd-alghtani246/" target="_blank">Visit My LinkedIn</a>';
+};
+;
+ document.getElementById('years1').onmouseover =function(){
+    this.textContent ='2019 - 2024';
+ }
+ document.getElementById('university').onmouseover =function(){
+    this.textContent ='Majmaah University';
+ };
+ document.getElementById('degree').onmouseover =function(){
+    this.textContent ='Bachelor in Computer Science';
+ };
+ document.getElementById('degree2').onmouseover =function(){
+    this.textContent ='Bachelor in Computer Science';
+    };
+    document.getElementById('years2').onmouseover =function(){
+        this.textContent ='2019 - 2024';
+    };
+    document.getElementById('avrage').onmouseover =function(){
+        this.textContent ='';
+    };
+    document.getElementById('skill1').onmouseover =function(){
+        this.textContent ='Working with HTML'};
+        document.getElementById('skill2').onmouseover =function(){
+            this.textContent ='Working with CSS and bootstrap'};
+            document.getElementById('skill3').onmouseover =function(){
+                this.textContent ='Working with javaScript'};
+                document.getElementById('skill4').onmouseover =function(){
+                    this.textContent ='Creating solutions'};
+                    document.getElementById('skill5').onmouseover =function(){
+                        this.textContent ='Achievement'};
+                        document.getElementById('skill6').onmouseover =function(){
+                            this.textContent ='Flexibility'};  document.getElementById('skill5').onmouseover =function(){
+                              this.textContent ='Achievement'};  
+ document.getElementById('lang1').onmouseover=function(){
+    this.textContent ='Arabic';
+ };
+ document.getElementById('lang2').onmouseover=function(){
+    this.textContent ='English';
+ };
+ document.getElementById('lang3').onmouseover=function(){
+    this.textContent ='';
+ };
+ document.getElementById('lang4').onmouseover=function(){
+    this.textContent ='';
+ };
